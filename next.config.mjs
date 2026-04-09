2c2a1fc7d68bf197fdfed6f7ab39973b5682ea2b
Shubhranshu Das <141610528+CrazyBong@users.noreply.github.com>
2026-04-10 00:24:22 +0530

    fix: authorize remote image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'jmqfvhyqrukavhkjochs.supabase.co',
+                port: '',
+                pathname: '/storage/v1/object/public/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'covers.openlibrary.org',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
